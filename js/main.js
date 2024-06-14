43880353cd6e9d8ef47947e243710ee567a0b4d5
akinari <akinari1215335@gmail.com>
2024-06-14 23:29:46 +0900

    style.scssにinputとlabelの右側に余白をつけた

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@
 
 {
   const todos = [
-    { title: 'aaa', isCompleted: false },
-    { title: 'bbb', isCompleted: false },
-    { title: 'ccc', isCompleted: false }
+    { title: 'aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa aaa', isCompleted: false },
+    { title: 'bbb', isCompleted: true},
+    { title: 'ccc', isCompleted: false },
+     { title: 'ddd', isCompleted: false }
   ];
   const renderTodo = (todo) => {
 //   <li>
