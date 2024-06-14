f2ce7bcf16b9aedb3d1777033e3c03dfed997ba1
akinari <akinari1215335@gmail.com>
2024-06-14 23:26:34 +0900

    jsからtodoリストを表示できるようにした

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
 'use strict'
+
+{
+  const todos = [
+    { title: 'aaa', isCompleted: false },
+    { title: 'bbb', isCompleted: false },
+    { title: 'ccc', isCompleted: false }
+  ];
+  const renderTodo = (todo) => {
+//   <li>
+//       <label>
+// <input type="checkbox">
+// <span>
+// aaa
+// </span>
+
+//       </label>
+//       <button>x</button>
+    //     </li>
+    const input = document.createElement('input')
+    input.type = 'checkbox';
+    input.checked = todo.isCompleted
+    const span = document.createElement('span')
+    span.textContent = todo.title;
+     const label = document.createElement('label')
+    label.appendChild(input)
+    label.appendChild(span)
+    const button = document.createElement('button')
+    button.textContent = 'x'
+    const li = document.createElement('li')
+    li.appendChild(label);
+    li.appendChild(button)
+    document.querySelector('#todos').appendChild(li)
+}
+
+  const renderTodos = () => {
+    todos.forEach((todo) => {
+      renderTodo(todo)
+    })
+  }
+  renderTodos();
+}
